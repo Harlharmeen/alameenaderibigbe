613f669a917ba702bbeadf13b31d8c90581b923d
Aderibigbe Al-ameen <alameenaderibigbe@gmail.com>
2026-03-06 16:18:33 -1200

    preview update

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,11 +11,40 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
 export const metadata: Metadata = {
-  title: "Aderibigbe Al-ameen",
-  description: "Brand Identity Design Portfolio",
-};
+  title: "Aderibigbe Al-ameen — Brand Identity Designer",
+  description:
+    "I help ambitious brands look refined, intentional, and positioned for long-term growth.",
+
+  openGraph: {
+    title: "Aderibigbe Al-ameen — Brand Identity Designer",
+    description:
+      "I help ambitious brands look refined, intentional, and positioned for long-term growth.",
+    url: "https://alameenaderibigbe.vercel.app/",
+    siteName: "Aderibigbe Al-ameen",
+    images: [
+      {
+        url: "/og-image.jpg",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Aderibigbe Al-ameen — Brand Identity Designer",
+    description:
+      "I help ambitious brands look refined, intentional, and positioned for long-term growth.",
+    images: ["/og-image.jpg"],
+  },
+}
+// export const metadata: Metadata = {
+//   title: "Aderibigbe Al-ameen",
+//   description: "Brand Identity Design Portfolio",
+// };
 
 export default function RootLayout({
   children,
